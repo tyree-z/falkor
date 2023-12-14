8a9f43ba8dc47cb14a53550a543ef68f701a9081
Tyree Zacharopoulos <tyree@tyree.ca>
2023-12-14 13:25:21 -0800

    feat(Debug): Logging
    App State Debug

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -1,21 +1,26 @@
 import type { ApplicationContract } from '@ioc:Adonis/Core/Application'
+import Logger from '@ioc:Adonis/Core/Logger'
 
 export default class AppProvider {
   constructor(protected app: ApplicationContract) {}
 
   public register() {
+    Logger.info('AppProvider registered')
     // Register your own bindings
   }
 
   public async boot() {
+    Logger.info('AppProvider booted')
     // IoC container is ready
   }
 
   public async ready() {
+    Logger.info('AppProvider ready')
     // App is ready
   }
 
   public async shutdown() {
+    Logger.info('AppProvider shutdown')
     // Cleanup, since app is going down
   }
 }
