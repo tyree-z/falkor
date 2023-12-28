2f3c869bb438113c321c4bf14cf4866635ebc615
Tyree Zacharopoulos <tyree@tyree.ca>
2023-12-27 16:24:52 -0800

    Update exception handling and request configuration

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -14,10 +14,26 @@
 */
 
 import Logger from '@ioc:Adonis/Core/Logger'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
+  protected disableStatusPagesInDevelopment: false
+  protected statusPages: {
+    '404': 'errors/not-found'
+  }
+
   constructor() {
     super(Logger)
+  }
+
+  public async handle(error: any, ctx: HttpContextContract) {
+    if (error.code === 'E_ROUTE_NOT_FOUND') {
+      return ctx.response.status(404).send({
+        message: 'Not Found',
+        code: error.code,
+      })
+    }
+    return super.handle(error, ctx)
   }
 }
